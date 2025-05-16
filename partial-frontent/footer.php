8dd95b79e0c996c056192e524f051c4e795f026f
lakshmn778180 <lk842569@gmail.com>
2025-05-16 12:55:39 +0530

    add new name

--- partial-frontent/footer.php
+++ partial-frontent/footer.php
@@ -1,5 +1,5 @@
 <section class="footer">
         <div class="container text-center">
-            <p>All rights reserved. Designed By <a href="#">Lakshman Jha</a></p>
+            <p>All rights reserved. Designed By <a href="#">Shreya kumari</a></p>
         </div>
     </section>
